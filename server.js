41a8af66dd24d6721e6a796206076b7733a33561
a-riceeater <elijb12@gmail.com>
2024-06-05 11:58:26 -0700

    disable for all

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ Date.prototype.subtractDays = function (days) {
 
 app.use(cookieParser());
 const csrfProtection = csrf({ cookie: true });
-app.use(csrfProtection);
+//app.use(csrfProtection);
 const loginLimiter = rateLimit({
     windowMs: 5 * 60 * 1000,
     max: 5, 
